1cd221f8a4fa362289382d893f646880c4aa9812
Eugenio Piasini <eugenio.piasini@gmail.com>
2021-03-31 23:32:20 +0200

    Fix typo in package metadata and add comments in cli test script

--- metex/test/test_cli_interface.sh
+++ metex/test/test_cli_interface.sh
@@ -17,5 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Metex. If not, see <https://www.gnu.org/licenses/>.
 
+# This script performs a simple sanity check for metex's command line
+# interface. We generate a single sample from a 10x10 white noise
+# texture and we save it to a temporary file on disk. We then check
+# that the file is present and finally we delete it.
 tfile=$(mktemp)
 metex --prefix $tfile. 10 && test -f $tfile.0.png && rm $tfile.0.png
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup (name=NAME,
            "Development Status :: 4 - Beta",
            "Intended Audience :: Science/Research",
            "Topic :: Scientific/Engineering",
-           "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
+           "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
            "Programming Language :: Python :: 3",
            "Operating System :: OS Independent",
            "Environment :: Console"           
